180eb6c5998448832d4491230ef6c328bb07f10a
Jason Mong <jason.mmong@ontariotechu.net>
2025-09-07 21:35:47 -0400

    completed merging and binary classification

--- dataConversion.py
+++ dataConversion.py
@@ -1,8 +1,13 @@
-import pandas as pd
+def mergeTables(adx, time_s, b_bands):
+    df = time_s.merge(b_bands, left_index = True, right_index = True, how = "inner")
+    df = df.merge(adx,left_index = True, right_index = True, how = "inner" )
+    return df
 
-#call teh client class instead of main once this workds
+def binaryConvert(table):
+    table["next_close"] = table["close"].shift(-1)
+    table["target"] = (table["next_close"] > table["close"]).astype(int)
 
+    #drop the last row since the shift 
+    table = table.iloc[:-1]
+    return table
 
-# def prepFrame(adx,b_bands, timeSeries) :
-#     finalFrame = pd.merge(adx,b_bands,timeSeries)
-#     print(finalFrame)
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import os
-#from dataConversion import prepFrame
+from dataConversion import mergeTables, binaryConvert
 from client import Client
 
 
 def run(ticker, interval):  
     client = Client.configure()
 
+    #retrieve data from client 
+    adx = client.get_adx(ticker, interval)
+    b_bands = client.get_bbands(ticker, interval)
+    t_series = client.get_tSeries(ticker, interval)
+    
+    #data prep
+    final_frame = mergeTables(adx, t_series, b_bands)
+    convertedFrame = binaryConvert(final_frame)
+    print(convertedFrame)
 
 if __name__ == "__main__":
     #Selection
